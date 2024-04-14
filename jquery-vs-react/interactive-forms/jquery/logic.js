1e35a8e1930c33254561d2e7ee4b677770da78aa
Deniss Suhhanov <desuhh@taltech.ee>
2024-04-14 18:04:05 +0300

    Add global styles, fix a bug

--- jquery-vs-react/interactive-forms/jquery/logic.js
+++ jquery-vs-react/interactive-forms/jquery/logic.js
@@ -77,7 +77,7 @@ function validateEmail(email) {
 function validateDate(dob) {
     const today = new Date();
     const birthDate = new Date(dob);
-    const age = today.getFullYear() - birthDate.getFullYear();
+    let age = today.getFullYear() - birthDate.getFullYear();
     const m = today.getMonth() - birthDate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
         age--;
